ef25e2e4ed088155ae7a1b40bda8eb6764178d95
siddhant <siddhantp736@gmail.com>
2021-09-20 18:59:38 +0530

    quiz part done

---
+++ quiz.js
@@ -0,0 +1,20 @@
+const quizform = document.querySelector('.quiz-form');
+const submitAnswerBtn = document.querySelector('#submit-answer-btn');
+const outputEl = document.querySelector('#output');
+
+const correctAnswers = ['90°', 'right angled'];
+
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formData = new FormData(quizform);
+  for (let value of formData.values()) {
+    if (value === correctAnswers[index]) {
+      score++;
+    }
+    index++;
+  }
+  outputEl.innerText = `Your Score is: ${score}`;
+}
+
+submitAnswerBtn.addEventListener('click', calculateScore);
